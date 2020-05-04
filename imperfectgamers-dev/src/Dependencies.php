335951ed65b9bdf593a2d8f0576eb21a616b5dc4
daiyaan <3193289+cheesea3@users.noreply.github.com>
2020-05-03 21:25:46 -0400

    replaced mustache with twig... big headache, old references were deprecated..

--- imperfectgamers-dev/src/Dependencies.php
+++ imperfectgamers-dev/src/Dependencies.php
@@ -15,7 +15,7 @@ $injector->define('Http\HttpRequest', [
 
 $injector->alias('Http\Response', 'Http\HttpResponse');
 $injector->share('Http\HttpResponse');
-$injector->alias('igmain\Template\Renderer', 'igmain\Template\MustacheRenderer');
+$injector->alias('igmain\Template\Renderer', 'igmain\Template\TwigRenderer');
 $injector->define('Mustache_Engine', [
     ':options' => [
         'loader' => new Mustache_Loader_FilesystemLoader(dirname(__DIR__) . '/templates', [
@@ -29,6 +29,11 @@ $injector->define('igmain\Page\FilePageReader', [
 
 $injector->alias('igmain\Page\PageReader', 'igmain\Page\FilePageReader');
 $injector->share('igmain\Page\FilePageReader');
+$injector->delegate('\Twig\Environment', function () use ($injector) {
+    $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/templates');
+    $twig = new \Twig\Environment($loader);
+    return $twig;
+});
 
 return $injector;
 
