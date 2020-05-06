1e9df1e446dec345f213976546edcf2c62c53987
daiyaan <3193289+cheesea3@users.noreply.github.com>
2020-05-06 06:00:35 -0400

    fixed css color issue when resizing, transitioned back to working on /imperfectgamers-dev/

--- imperfectgamers-dev/inc/assets/components/footer/footer.php
+++ imperfectgamers-dev/inc/assets/components/footer/footer.php
@@ -1,51 +1,53 @@
 <!-- Footer -->
-    <div class="container ">
-        <div class="row text-center text-xs-center text-sm-left text-md-left">
-            <div class="col-xs-12 col-sm-4 col-md-4">
-                <h5>Directory</h5>
-                <ul class="list-unstyled quick-links">
-                    <li><a href="/"><i class="fa fa-angle-double-right"></i> Home</a></li>
-                    <li><a href="/records"><i class="fa fa-angle-double-right"></i> Records</a></li>
-                    <li><a href="/activity"><i class="fa fa-angle-double-right"></i> Activity</a></li>
-                    <li><a href="/bans"><i class="fa fa-angle-double-right"></i> Bans</a></li>
-                    <li><a href="/store"><i class="fa fa-angle-double-right"></i> Store</a></li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-4 col-md-4">
-                <h5>Documentation</h5>
-                <ul class="list-unstyled quick-links">
-                    <li><a href="/forum/Thread-Official-Community-Information"><i class="fa fa-angle-double-right"></i> Community Information</a></li>
-                    <li><a href="/forum/Thread-Official-Community-Rules-Guidelines"><i class="fa fa-angle-double-right"></i> Community Rules & Guidelines</a></li>
-                    <li><a href="/forum/Thread-Official-SourceMod-Commands"><i class="fa fa-angle-double-right"></i> Sourcemod Commands</a></li>
-                    <li><a href="/store/privacy.php" title="You hereby agree that by accepting this TOS, you know that your contract with ImperfectGamers.org also correlates to the server and any service that is owned by us."><i class="fa fa-angle-double-right"></i> Terms of Service</a></li>
-                    <li><a href="/store/tos.php" title="This page governs the manner in which Imperfect Gamers operates."><i class="fa fa-angle-double-right"></i> Privacy Policy</a></li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-4 col-md-4">
-                <h5>Private</h5>
-                <ul class="list-unstyled quick-links">
-                    <li><a href="https://www.imperfectgamers.org/forum/Thread-Using-the-Admin-Panel"><i class="fa fa-angle-double-right"></i> Using the Admin Panel</a></li>
-                    <li><a href="https://www.imperfectgamers.org/forum/Thread-Advanced-Rules-Menu"><i class="fa fa-angle-double-right"></i> Advanced Rules Menu</a></li>
-                    <li><a href="https://www.imperfectgamers.org/forum/Thread-SourceMod-Staff-Commands"><i class="fa fa-angle-double-right"></i> Sourcemod Staff Commands</a></li>
-                </ul>
-            </div>
+<div class="container">
+    <div class="row text-center text-xs-center text-sm-left text-md-left">
+        <div class="col-xs-12 col-sm-4 col-md-4">
+            <h5>Directory</h5>
+            <ul class="list-unstyled quick-links">
+                <li><a href="/"><i class="fa fa-angle-double-right"></i> Home</a></li>
+                <li><a href="/records"><i class="fa fa-angle-double-right"></i> Records</a></li>
+                <li><a href="/activity"><i class="fa fa-angle-double-right"></i> Activity</a></li>
+                <li><a href="/bans"><i class="fa fa-angle-double-right"></i> Bans</a></li>
+                <li><a href="/store"><i class="fa fa-angle-double-right"></i> Store</a></li>
+            </ul>
         </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
-                <ul class="list-unstyled list-inline social text-center">
-                    <li class="list-inline-item"><a href="/steam" target="_blank"><i class="fab fa-steam-square"></i></a></li>
-                    <li class="list-inline-item"><a href="/instagram" target="_blank"><i class="fab fa-instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="/discord" target="_blank"><i class="fab fa-discord"></i></a></li>
-                    <li class="list-inline-item"><a href="mailto:support@imperfectgamers.org" target="_blank"><i class="fa fa-envelope"></i></a></li>
-                </ul>
-            </div>
-            <hr>
+        <div class="col-xs-12 col-sm-4 col-md-4">
+            <h5>Documentation</h5>
+            <ul class="list-unstyled quick-links">
+                <li><a href="/forum/Thread-Official-Community-Information"><i class="fa fa-angle-double-right"></i> Community Information</a></li>
+                <li><a href="/forum/Thread-Official-Community-Rules-Guidelines"><i class="fa fa-angle-double-right"></i> Community Rules & Guidelines</a></li>
+                <li><a href="/forum/Thread-Official-SourceMod-Commands"><i class="fa fa-angle-double-right"></i> Sourcemod Commands</a></li>
+                <li><a href="/store/privacy.php" title="You hereby agree that by accepting this TOS, you know that your contract with ImperfectGamers.org also correlates to the server and any service that is owned by us."><i class="fa fa-angle-double-right"></i> Terms of Service</a></li>
+                <li><a href="/store/tos.php" title="This page governs the manner in which Imperfect Gamers operates."><i class="fa fa-angle-double-right"></i> Privacy Policy</a></li>
+            </ul>
         </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-                <p><u><a href="/">Imperfect Gamers</a></u> is a service of Imperfect and Company, LLC.</p>
-                <p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://imperfectcreators.org/" target="_blank">Imperfect and Company</a></p>
-            </div>
+        <div class="col-xs-12 col-sm-4 col-md-4">
+            <h5>Private</h5>
+            <ul class="list-unstyled quick-links">
+                <li><a href="https://www.imperfectgamers.org/forum/Thread-Using-the-Admin-Panel"><i class="fa fa-angle-double-right"></i> Using the Admin Panel</a></li>
+                <li><a href="https://www.imperfectgamers.org/forum/Thread-Advanced-Rules-Menu"><i class="fa fa-angle-double-right"></i> Advanced Rules Menu</a></li>
+                <li><a href="https://www.imperfectgamers.org/forum/Thread-SourceMod-Staff-Commands"><i class="fa fa-angle-double-right"></i> Sourcemod Staff Commands</a></li>
+            </ul>
         </div>
     </div>
-<!-- ./Footer -->
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+            <ul class="list-unstyled list-inline social text-center">
+                <li class="list-inline-item"><a href="/steam" target="_blank"><i class="fab fa-steam-square"></i></a></li>
+                <li class="list-inline-item"><a href="/instagram" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                <li class="list-inline-item"><a href="/discord" target="_blank"><i class="fab fa-discord"></i></a></li>
+                <li class="list-inline-item"><a href="mailto:support@imperfectgamers.org" target="_blank"><i class="fa fa-envelope"></i></a></li>
+            </ul>
+        </div>
+        <hr>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+            <p style="color: gray"><a href="abc.html"
+                                      onMouseOver="this.style.color='#gray'"
+                                      onMouseOut="this.style.color='#gray'" >Imperfect Gamers</a> is a service of Imperfect and Company, LLC.</p>
+            <p class="h6">Copyright 2020 ©<a class="ml-2" href="https://imperfectcreators.org/" target="_blank">Imperfect and Company</a> All right Reversed.<a class="ml-2" style="color: white" href="https://imperfectcreators.org/" target="_blank">Privacy Policy</a></p>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
