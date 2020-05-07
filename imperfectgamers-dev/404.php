43a65cf9407a933034563495bf70c06bb2bb1a12
daiyaan <3193289+cheesea3@users.noreply.github.com>
2020-05-07 06:18:46 -0400

    fixed link for 404 page, used a script as a temp hotfix until i have time to make something more elaborate.

--- imperfectgamers-dev/404.php
+++ imperfectgamers-dev/404.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER'])){
     $refurl = parse_url($_SERVER['HTTP_REFERER']); // use the parse_url() function to create an array containing information about the domain
-    if($refurl['host'] == "cutlery-in-the-toaster.com"){
+    if($refurl['host'] == "imperfectgamers.org"){
         echo "Please email contact@imperfectgamers.org and let us know about the dead link on our site.";
     }
     else{
