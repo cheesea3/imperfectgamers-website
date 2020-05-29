b9bf369ad503c4498513638aae4bf859acb77af1
daiyaan <3193289+cheesea3@users.noreply.github.com>
2020-05-28 22:45:28 -0400

    collect and insert into the database

--- imperfectgamers-dev/CRUD/index.php
+++ imperfectgamers-dev/CRUD/index.php
@@ -5,25 +5,29 @@
         LEARNING CRUD
     </title>
     <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-
-
-<form action="#" method="POST">
+<?php require_once 'process.php';?>
+<div class="row justify-content-center">
+<form action="process.php" method="POST">
+    <div class="form-group">
     <label>Name</label>
-    <input type="type" name="name" value="Enter your name">
+    <input type="type" name="name" class="form-control" value="Enter your name">
+    </div>
+    <div class="form-group">
     <label>Location</label>
-    <input type="text" name="location" value=""Enter your location">
-    <button type="submit" name="save">Save</button>
+    <input type="text" name="location" class="form-control" value="Enter your location">
+    </div>
+    <div class="form-group">
+    <button type="submit" class="btn btn-primary" name="save">Save</button>
+    </div>
 </form>
+</div>
 
 
 
