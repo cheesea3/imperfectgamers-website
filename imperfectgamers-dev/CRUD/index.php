ca4dd4a0a85d3f0b4f8b4a46831e2198fd0ee74a
daiyaan <3193289+cheesea3@users.noreply.github.com>
2020-05-28 22:03:28 -0400

    Made a table for personal reference, also to remind myself from the very beginning on what exactly i'm going to use in it's actual usage.

--- imperfectgamers-dev/CRUD/index.php
+++ imperfectgamers-dev/CRUD/index.php
@@ -15,14 +15,79 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-<form action="" method="POST">
+
+
+<form action="#" method="POST">
     <label>Name</label>
     <input type="type" name="name" value="Enter your name">
     <label>Location</label>
     <input type="text" name="location" value=""Enter your location">
-<button type="submit" name="save">Save</button>
+    <button type="submit" name="save">Save</button>
 </form>
 
-</form>
+
+
+
+<style>
+    td, th {
+    padding: 15px;
+    }
+</style>
+<table>
+    <caption>What i'm going to use</caption>
+<tr>
+    <th>Elements</th>
+    <th>Class</th>
+    <th>What does it do</th>
+</tr>
+    <tr>
+        <td>div</td>
+        <td>Justify-content-center</td>
+        <td>aligns flexible content to the center using it's available space (horizontally).
+        </td>
+    </tr>
+    <tr>
+        <td>div</td>
+        <td>Form group</td>
+        <td>Used to wrap labels and form controls</td>
+    </tr>
+    <tr>
+        <td>div</td>
+        <td>alert alert-warning</td>
+        <td>Wrapping this around the text makes the design suitable for alerts</td>
+    </tr>
+    <tr>
+        <td>div</td>
+        <td>alert alert-danger</td>
+        <td>Wrapping this around the text makes the design suitable for alerts</td>
+    </tr>
+    <tr>
+        <td>div</td>
+        <td>alert alert-success</td>
+        <td>Wrapping this around the text makes the design suitable for alerts</td>
+    </tr>
+    <tr>
+        <td>div</td>
+        <td>row</td>
+        <td>Wrappers for columns, adds a gutter in between each line and if you want a new row,</td>
+    </tr>
+    <tr>
+        <td>input</td>
+        <td>form-control</td>
+        <td>helps with the appearance, focus state, sizing, and more. applied on &lt;input&gt;, &lt;select&gt;, and &lt;textarea&gt;.</td>
+    </tr>
+    <tr>
+        <td>input</td>
+        <td>btn btn-primary</td>
+        <td>Wrapping this around the button makes the design suitable for alerts</td>
+    </tr>
+    <tr>
+        <td>input</td>
+        <td>btn btn-info</td>
+        <td>Wrapping this around the button makes the design suitable for alerts</td>
+    </tr>
+</table>
+
+
 </bodY>
 </html>
