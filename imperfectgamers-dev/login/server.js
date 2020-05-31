0779becdeea19a993ebb566fd25cb5312ebcdaf6
daiyaan <3193289+cheesea3@users.noreply.github.com>
2020-05-31 00:05:46 -0400

    using TOKEN BASED AUTH (not passport)

--- imperfectgamers-dev/login/server.js
+++ imperfectgamers-dev/login/server.js
@@ -1,5 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.listen(3000)
+const app = express();
 
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Imperfect Gamers" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
