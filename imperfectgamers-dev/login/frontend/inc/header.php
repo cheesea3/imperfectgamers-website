80dcfb1de736711d3405c43317532f052aea76c8
daiyaan <3193289+cheesea3@users.noreply.github.com>
2020-06-03 21:47:30 -0400

    discovery -> finished Laravel x Vue installed Setup login, roles -> done.

--- imperfectgamers-dev/login/frontend/inc/header.php
+++
@@ -1,7 +0,0 @@
-<div class="banner" style="text-align: center;">
-    <div class="container" style="padding: 20px">
-
-        <object data="https://cdn.imperfectgamers.org/inc/assets/svg/text.svg" height="24px"></object>
-
-    </div>
-</div>
