c26b6100a0b2dfe6f910acc2ac0b80c726f6a08e
daiyaan <3193289+cheesea3@users.noreply.github.com>
2020-06-03 23:38:16 -0400

    setup function to check if a user has specified role

---
+++ imperfectgamers-dev/laravel/resources/views/nopermission.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        Sorry, you do not have permission to access this page. <br>
+                        Error 1001: Access denied
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
