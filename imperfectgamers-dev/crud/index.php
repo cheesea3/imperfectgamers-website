3757a7d7c0c5fe69e0ff2d80a42769e6374c3fdf
daiyaan <3193289+cheesea3@users.noreply.github.com>
2020-06-30 20:30:09 -0400

    layout

--- imperfectgamers-dev/crud/index.php
+++ imperfectgamers-dev/crud/index.php
@@ -1,35 +1,64 @@
 <html>
-<head>
-
- <head>
-
-
-<style>
-html {
-    margin: 100px;
-
-}
-    th {
-    padding: 15px;
-}
-
-</style>
-
 <?php include_once ("conn.php");
 ?>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+    * {
+        box-sizing: border-box;
+    }
+    .header {
+        border: 1px solid red;
+        padding: 15px;
+        text-align: center;
+    }
+    .main {
+        width: 75%;
+        float: left;
+        border: 1px solid red;
+        padding: 15px
+
+    }
+    .sidebar {
+        width: 25%;
+        float: left;
+        border: 1px solid red;
+        padding: 15px
+    }
+</style>
+</head>
 
 
-<table>
-<tr>
-    <th>Column name</th>
-    <th>Column name</th>
-    <th>Column name</th>
-</tr>
-    <tr>
-        <td>Data 1</td>
-        <td>Data 2</td>
-        <td>Data 3</td>
-    </tr>
-</table>
+<body>
+<div class="header">
+<h1>Imperfect Gamers</h1>
+</div>
+
+
+<div class="sidebar">
+    <h1>Search</h1>
+</div>
+
+     <div class="main">
+         <h1>Stats</h1>
+     <table>
+             <tr>
+                 <th>Player</th>
+                 <th>Joined</th>
+                 <th>Last Seen</th>
+             </tr>
+             <tr>
+                 <td>Jumpman</td>
+                 <td>6/29/2020</td>
+                 <td>6/30/2020</td>
+             </tr>
+         </table>
+     </div>
+</body>
+
+
+
+
+
 
 </html>
